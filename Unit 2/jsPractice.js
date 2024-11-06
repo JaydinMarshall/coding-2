51bcd8ab7b7080790224d958af3d85bea721e40a
JaydinMarshall <jmarshall027@boyslatin.net>
2024-11-06 18:35:43 +0000

    asyncactivitynov1stcompleted

---
+++ Unit 2/jsPractice.js
@@ -0,0 +1,19 @@
+// two forward = this is a comment
+// String data type
+// Strings are pieces of data that are wrapped around
+// quotation marks
+
+// Boolean
+// a boolean is a piece of
+// data that represents a true or false value
+
+// variables
+// variables are constructs that store data
+var email
+var password
+
+//temporary variable=this data can change
+let image
+
+// fixed variable= cannot/ should not change
+const password
