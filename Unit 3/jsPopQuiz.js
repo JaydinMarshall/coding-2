ac4d1b523debdf71dbf0fc8b798cfb0c6ad869b7
JaydinMarshall <jmarshall027@boyslatin.net>
2024-11-07 18:40:25 +0000

    popquizcompleted

---
+++ Unit 3/jsPopQuiz.js
@@ -0,0 +1,40 @@
+// 1. How do you declare a variable in JavaScript? Please write your answer using JavaScript code. 
+
+// By using the keywords var, let, or const.
+
+// 2. How would you declare a variable where the data WILL NOT change in JavaScript? Please write your answer using JavaScript code. 
+
+//by using the keyword "const" only.
+
+// 3. Identify the following JavaScript operators and describe when you would use it? Please write your answers using the string data type. 
+
+// "=" (Assignment Opperator) assign value, "+" (Addition Operator) adds value, "*" (Multiplication Operator) multiply value, ">" (Comparison Operator) compares value.
+
+// 4. Describe a scenario where you would use a comparison operator, and then provide a code example based on the scenerio you provided. 
+
+//Using the comparison operator would help in comparing a set of differnt prices to a given object. 
+let meal1 = "20.50";
+let meal2 = "5.00";
+let result = meal1 < meal2;
+
+// 5. Describe a scenario where you would use a logical operator, and then provide a code example based on the scenerio you provided. 
+
+//I would use a logical operator to determine the logic between a set of variables or values, such as prices.
+(x < 10 && y > 1)
+
+// 6. Create three variables where when combined will result in the value of 36. Please use the console.log () to out put your result. 
+
+var a="12"
+var b="12"
+var c="12"
+console.log(12+12+12)
+
+// 7. Fill in the variable that will output the appropriate result. 
+
+var x ='Please replace this text with your answer.'
+var y = x *2
+
+// Your console log should output the value 28.
+console.log(y)
+
+// 8. What does it "concatenation" mean? Please write your response as a string and also provide a code example of two variables being concatenated and output the result using the console.log() function. 
