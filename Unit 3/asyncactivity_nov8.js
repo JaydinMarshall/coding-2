451286a348a16ad4a0911101b7674fae5198d41c
JaydinMarshall <jmarshall027@boyslatin.net>
2024-11-12 18:40:26 +0000

    asyncactivity8completed

---
+++ Unit 3/asyncactivity_nov8.js
@@ -0,0 +1,26 @@
+// String Concatenation is when you combine two strings using an operator (+)
+a = 'this is'
+b = "coding class"
+c = a + b
+
+'conts'- variables that will not change
+'let'- variables that will change
+'var' - universal/default variable 
+
+'1. Variable names cannot start with a number' 
+'2. Variables cannot contain a space '
+'3. variable names cannot be reserved words'
+
+// - Camel Case - every word after the first is capitalized
+// - Snake case - every word is seperated by an "_"
+// - Pascal Case - every word is capitalized 
+
+let a = 350
+let b = a * 2
+console.log(b)
+
+x = 10
+y = 2
+z = y*2
+console.log( x > 1 && z==4)
+
