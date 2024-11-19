842034a3fc90b269af3334120e5d05a960322a81
JaydinMarshall <jmarshall027@boyslatin.net>
2024-11-19 18:41:03 +0000

    async completed

---
+++ Unit 3/asynxReviewNov15.js
@@ -0,0 +1,7 @@
+1. Logical Operator
+
+2. Comparison Operator
+
+3.Assignment Operator
+
+4.= - Assignment, && - Logical, ll (double line) - Logical, == - Comparison
---
+++ Unit 3/functionPractice/function.js
@@ -0,0 +1,23 @@
+// what is a function?
+'a set of instructions that performs a specifc task or job'
+
+//there are two variations of functions:
+// built-in functions: these are function instructions that are already built in the programming language and all you need to do is pass it data
+
+console.log()
+
+
+
+// user defined functions: these are custom instructions that are written by engineers 
+
+//how do we write a function - function syntax
+// declare function
+function exampleFunction(){
+    alert('you are now connected to the JS file')
+    console.log('step 1');
+    console.log('step 2');
+    console.log('step 3'); 
+}
+
+//call function
+exampleFunction();
