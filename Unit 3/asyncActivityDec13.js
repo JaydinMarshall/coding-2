7889017e2c24e8f14e2c217abe2c2cc1239481d6
JaydinMarshall <jmarshall027@boyslatin.net>
2024-12-13 18:41:21 +0000

    dec13async half way completed

---
+++ Unit 3/asyncActivityDec13.js
@@ -0,0 +1,26 @@
+1.
+
+function states(landmark){
+    if(number lacks a "-"){
+        console.log("your number is positive")
+    }
+    else if(number possesses a "-"){
+        console.log("number is negative")
+    }
+    else if(number is "0"){
+        console.log("number is zero")
+    }
+}
+
+2.
+
+function movieticketprice(age) {
+    if (age <= 10) {
+        return "$5.00";
+} else if (age >= 16 && age < 20) {
+        return "$10.00";
+} else if (age >= 20 &&)
+        }
+    }
+}
+
