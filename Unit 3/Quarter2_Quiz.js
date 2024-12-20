6a4e5a259eb969e55ced10c9c1e2ab9220ba5ba6
JaydinMarshall <jmarshall027@boyslatin.net>
2024-12-20 19:05:47 +0000

    QUIZ COMPLETED

---
+++ Unit 3/Quarter2_Quiz.js
@@ -0,0 +1,71 @@
+// a set of instructions that performs a specifc task or job
+
+// function parameters are names listed in the defintion of a function
+// function arguments are the real values that are passed to, and recieved by, the function
+
+// a code that allows you to perform different actions for different decisions 
+
+// used to store a whole number without decimals (raning from signed or unsigned)
+
+// a piece of data that represents a true or false value
+
+// comparison operator
+
+// assignment operator
+
+
+function PasswordValidity(password){
+    if(passcode > 10){
+        console.log("password is too long")
+    }
+    else if(password < 4){
+        console.log("password is too short")
+    }
+    else if(password === 4){
+        console.log("password is valid")
+    }
+    else if(password === 5){
+        console.log("password is valid")
+    }
+    else if(password === 6){
+        console.log("password is valid")
+    }
+    else if(password === 7){
+        console.log("password is valid")
+    }
+    else if(password === 8){
+        console.log("password is valid")
+    }
+    else if(password === 9){
+        console.log("password is valid")
+    }
+    else if(password === 10){
+        console.log("password is valid")
+    }
+
+}
+    PasswordValidity()
+
+
+
+
+
+
+
+    function FederalIncomeTax(annualincome){
+        if(annualincome<= 12000){
+            console.log('you will be taxed 10%, plus an additional 3% for the state.')
+        }
+        else if(annualincome>= 12001 && <= 50000){
+            console.log("you will be taxed 12%, plus an additional 3% for the state.")
+        }
+        else if(annualincome>= 50001 && <= 103000){
+        console.log("you will be taxed 22%, plus an additional 3% for the state.")
+        }
+        else if(annualincome>= 103001 && <= 198000){
+        console.log("you will be taxed 23%, plus an additional 3% for the state.")
+
+    }
+
+}   FederalIncomeTax() 
+
---
+++ Unit 3/holidayExtraCredit.js
@@ -0,0 +1,12 @@
+function holidaySchedulePlanner(date){
+    if(12-21-24){
+        console.log("number is positive")
+    }
+    else if(number < 0){
+        console.log("number is negative")
+    }
+    else if(number === 0){
+        console.log("number is zero")
+    }
+}
+positiveornegative()
